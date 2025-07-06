c2d414f7b2f4031ff64615b5d61b229d97f2f5f7
zamtiyo <75232355+zamtiyo@users.noreply.github.com>
2025-07-06 11:35:12 +0700

    Create 131.py

---
+++ 131.py
@@ -0,0 +1,8 @@
+#hal 131
+def display_message():
+    print(f"Hello")
+display_message()
+
+def favorite_book(title):
+    print(f"My favorite book is {title}")
+favorite_book("Alice in borderland")

e87c666a072c80c493ca6a8835f464ce75a40bfe
zamtiyo <75232355+zamtiyo@users.noreply.github.com>
2025-07-09 21:37:29 +0700

    Create 141.py

---
+++ 141.py
@@ -0,0 +1,14 @@
+#hal 141
+def city_country(city,country):
+    full = {city}, {country}
+    return full
+
+print(city_country("Jakarta","Indonesia"))
+print(city_country("Bekasi","Indonesia"))
+print(city_country("Ngawi","Indonesia"))
+
+def make_album(artist,title):
+    music_album = [artist,title]
+    return music_album
+
+print(make_album("jojo","d4c"))

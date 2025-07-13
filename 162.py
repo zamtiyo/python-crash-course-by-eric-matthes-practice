ff8321c546faa6b5b35fa8a9508e621c7a461053
zamtiyo <75232355+zamtiyo@users.noreply.github.com>
2025-07-13 13:24:37 +0700

    Update 162.py

--- 162.py
+++ 162.py
@@ -1,3 +1,4 @@
+#Restaurant
 class Restaurant:
     def __init__(self, name, cuisine_type):
         self.name = name
@@ -15,3 +16,23 @@ r2 = Restaurant('taco bell', 'mexican')
 r2.describe_restaurant()
 r2.open_restaurant()
 
+#User
+#hal 162
+class User:
+    def __init__(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+        print(f"The first name of the user is {self.first_name} the last name is {self.last_name}")
+
+    def greet_user(self):
+        print(f"hello {self.first_name} {self.last_name}")
+
+user_1 = User("Bian","Nigga")
+user_1.describe_user()
+user_1.greet_user()
+
+user_2 = User("Oop","Anjing")
+user_2.describe_user()
+user_2.greet_user()
